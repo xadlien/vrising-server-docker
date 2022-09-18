573eba158c69b52ce602a5fc1c1f9e2e7ae5dd76
Daniel Martin <xadlien@gmail.com>
2022-09-17 21:44:20 -0400

    new save paths

--- start_server_docker.sh
+++ start_server_docker.sh
@@ -7,8 +7,8 @@ echo "Starting Xvfb"
 Xvfb :0 -screen 0 1024x768x16 &
 echo "Launching wine64 V Rising"
 echo " "
-DISPLAY=:0.0 wine64 VRisingServer.exe -persistentDataPath '.\save-data' -serverName 'Knorr Clan V Rising Server' -saveName 'world1' -logFile '.\logs\VRisingServer.log' -gamePort 27015 -queryPort 27016 2>&1
-/usr/bin/tail -f /mnt/vrising/persistentdata/VRisingServer.log
+DISPLAY=:0.0 wine64 VRisingServer.exe -persistentDataPath '/vrising/save-data' -serverName 'Knorr Clan V Rising Server' -saveName 'world1' -logFile '/vrising/logs/VRisingServer.log' -gamePort 27015 -queryPort 27016 2>&1
+/usr/bin/tail -f /vrising/logs/VRisingServer.log
 
 echo "Exiting.."
 exit
